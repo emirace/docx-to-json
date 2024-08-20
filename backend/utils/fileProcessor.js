42807e37b991d2847f6f218d6438791141c532a3
emirace <emmanuelakwuba57@gmail.com>
2024-08-20 10:58:45 +0100

    ik

--- backend/utils/fileProcessor.js
+++ backend/utils/fileProcessor.js
@@ -1,91 +1,63 @@
 const mammoth = require("mammoth");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 const processDocxFile = async (filePath) => {
-  const { value: htmlContent, messages } = await mammoth.convertToHtml({
-    path: filePath,
+  const buffer = fs.readFileSync(filePath);
+
+  // Custom style map
+  const styleMap = [
+    "p[style-name='Cover Product title'] => p.cover-product-title",
+    "p[style-name='Document title'] => p.document-title",
+    "p[style-name='Cover Job details'] => p.cover-job-details",
+    "p[style-name='TOC Heading'] => p.toc-heading",
+    "p[style-name='TGT_Body'] => p.tgt-body",
+    "p[style-name='toc 1'] => p.toc-1",
+    "p[style-name='toc 2'] => p.toc-2",
+    "p[style-name='TGT_HEADING2'] => h2.tgt-heading2",
+    "p[style-name='List Bullet'] => ul > li",
+    "p[style-name='caption'] => p.caption",
+    "p[style-name='TGT_Caption'] => p.tgt-caption",
+    "p[style-name='TGT_HEADING1'] => h1.tgt-heading1",
+    "p[style-name='Body Text'] => p.body-text",
+    "r[style-name='Body Text Char'] => span.body-text-char",
+    "r[style-name='Caption Char'] => span.caption-char",
+    "r[style-name='Heading 1 Char'] => span.heading1-char",
+  ];
+
+  const result = await mammoth.convertToHtml({ buffer }, { styleMap });
+
+  // Log any warnings
+  result.messages.forEach((message) => {
+    console.warn(message);
   });
 
-  //   console.log(htmlContent);
-  console.log(messages);
-  const jsonData = parseHtmlToJson(htmlContent);
+  const jsonData = parseHtmlToJson(result.value);
   return jsonData;
 };
 
 const parseHtmlToJson = (htmlContent) => {
   const $ = cheerio.load(htmlContent);
 
-  const headers = [];
-  const paragraphs = [];
-  const lists = [];
-  const images = [];
-  const tables = [];
+  const buildJsonStructure = (element) => {
+    const children = [];
 
-  $("h1, h2, h3, h4, h5, h6").each((i, el) => {
-    console.log($(el).attr());
-    headers.push({
-      tag: el.tagName,
-      text: $(el).text(),
-      color: $(el).css("color"),
-      style: $(el).attr("style"),
-    });
-  });
-
-  $("p").each((i, el) => {
-    paragraphs.push({
-      text: $(el).text(),
-      style: $(el).attr("style"),
-      color: $(el).css("color"),
-    });
-  });
-
-  $("ul, ol").each((i, el) => {
-    const items = [];
-    $(el)
-      .find("li")
-      .each((j, li) => {
-        items.push($(li).text());
+    $(element)
+      .children()
+      .each((_, child) => {
+        children.push(buildJsonStructure(child));
       });
-    lists.push({
-      type: el.tagName,
-      items,
-    });
-  });
 
-  $("img").each((i, el) => {
-    images.push({
-      src: $(el).attr("src"),
-      alt: $(el).attr("alt"),
-      caption: $(el).next("figcaption").text(),
-    });
-  });
-
-  $("table").each((i, el) => {
-    const rows = [];
-    $(el)
-      .find("tr")
-      .each((j, row) => {
-        const cells = [];
-        $(row)
-          .find("td, th")
-          .each((k, cell) => {
-            cells.push($(cell).text());
-          });
-        rows.push(cells);
-      });
-    tables.push({
-      caption: $(el).prev("caption").text(),
-      rows,
-    });
-  });
-
-  return {
-    headers,
-    paragraphs,
-    lists,
-    images,
-    tables,
+    return {
+      tag: $(element).get(0).tagName,
+      text: $(element).text(),
+      style: $(element).attr("class") || "",
+      children: children.length > 0 ? children : undefined,
+    };
   };
+  const jsonResult = buildJsonStructure($.root());
+
+  return jsonResult;
 };
 
 module.exports = { processDocxFile };
